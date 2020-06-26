c02cf20151cbd9c208523d7daf15bb179564ff3f
arnie b <arnold.bwaila@gmail.com>
2020-06-26 13:33:04 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change
     create_table :actor do |t|
-      #primary key of :id is created for us!
       t.string :first_name
+      t.string :last_name
     end
   end
 end
