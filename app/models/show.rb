b0eb2c477b9638add0287cc528ccbd6bd3d65390
arnie b <arnold.bwaila@gmail.com>
2020-06-26 13:58:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
-  has_many :artists, through: :characters
+  has_many :actors, through: :characters
+  def actor_list
+    self.characters.collect do |c|
+      c.actor.full_name
+    end
 end
