bbc48f8f7d1288a78e2593864752e8b8761bb27f
arnie b <arnold.bwaila@gmail.com>
2020-06-26 14:15:28 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -2,7 +2,6 @@ class AddColumnsToShows < ActiveRecord::Migration[5.1]
   def change
       add_column :shows, :day, :string
       add_column :shows, :genre, :string
-      add_column :shows, :day, :string
-      add
+      add_column :shows, :season, :string
   end
 end
